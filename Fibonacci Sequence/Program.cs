e10246a94f7c5f1d81eb21b67103c1b7b333a850
Ismael <42499036+goismael@users.noreply.github.com>
2018-11-05 10:25:55 -0500

    Add files via upload

---
+++ Fibonacci Sequence/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci_Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] FibNum = new int[21];
+            FibNum[0] = 0;
+            FibNum[1] = 1;
+            for (int counter = 2; counter <= 20; counter++)
+            {
+                FibNum[counter] = FibNum[counter - 2] + FibNum[counter - 1];
+                Console.WriteLine(FibNum[counter]);
+            }
+            Console.Read();
+        }
+        
+    }
+}
